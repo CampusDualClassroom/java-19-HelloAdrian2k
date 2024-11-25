cb9a3c02aa44cf7dc3ba7f7af48371c946a7f2b5
Adrián <mz.adriaan@gmail.com>
2024-11-25 18:54:13 +0100

    Ejercicio 19 completado.

--- src/main/java/com/campusdual/classroom/Exercise19.java
+++ src/main/java/com/campusdual/classroom/Exercise19.java
@@ -14,18 +14,18 @@ public class Exercise19 {
             sb.append(stringTriMatrixRow(intArrayTri, i));
             sb.append("\n");
         }
-        sb.delete(sb.length()-1, sb.length());
+        sb.delete(sb.length() - 1, sb.length());
         return sb.toString();
 
     }
 
     private static String stringTriMatrixRow(int[][][] intArrayTri, int row) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < intArrayTri.length-1; i++){
+        for (int i = 0; i < intArrayTri.length - 1; i++) {
             sb.append(getUnidimensionalString(intArrayTri[i][row]));
             sb.append("   ");
         }
-        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length-1][row]));
+        sb.append(getUnidimensionalString(intArrayTri[intArrayTri.length - 1][row]));
 
         return sb.toString();
     }
@@ -42,9 +42,9 @@ public class Exercise19 {
     public static int[][] flatTridimensionalArray(int[][][] intArrayTri) {
         int[][] flatTriArray = new int[intArrayTri[0].length][intArrayTri[0][0].length];
         int sum = 0;
-        for(int d = 0; d < flatTriArray.length; d++){
-            for(int k = 0; k < flatTriArray[d].length; k++){
-                for(int j = 0; j < intArrayTri.length; j++){
+        for (int d = 0; d < flatTriArray.length; d++) {
+            for (int k = 0; k < flatTriArray[d].length; k++) {
+                for (int j = 0; j < intArrayTri.length; j++) {
                     sum += intArrayTri[j][d][k];
                 }
                 flatTriArray[d][k] = sum;
@@ -57,8 +57,8 @@ public class Exercise19 {
     public static String getBidimensionalString(int[][] intArrayBi) {
         StringBuilder builder = new StringBuilder();
         int c = 1;
-        for(int i = 0; i < intArrayBi.length; i++){
-            for(int a = 0; a < intArrayBi[i].length; a++){
+        for (int i = 0; i < intArrayBi.length; i++) {
+            for (int a = 0; a < intArrayBi[i].length; a++) {
                 builder.append((a < intArrayBi.length - 1) ? c++ + " " : (i < intArrayBi[i].length - 1) ?
                         c++ + "\n" : c++);
             }
@@ -68,7 +68,7 @@ public class Exercise19 {
 
     public static String getUnidimensionalString(int[] uniArray) {
         StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < uniArray.length; i++){
+        for (int i = 0; i < uniArray.length; i++) {
             builder.append((i < uniArray.length - 1) ? uniArray[i] + " " : uniArray[i]);
         }
         return builder.toString();
@@ -77,7 +77,7 @@ public class Exercise19 {
     public static int[] createAndPopulateUnidimensionalArray(int columns) {
         int[] arrayTemplate = new int[columns];
         int cont = 1;
-        for(int c = 0; c < columns; c++){
+        for (int c = 0; c < columns; c++) {
             arrayTemplate[c] = cont;
             cont++;
         }
@@ -87,27 +87,27 @@ public class Exercise19 {
     public static int[][] createAndPopulateBidimensionalArray(int rows, int columns) {
         int[][] bidArrayTemplate = new int[rows][columns];
         int cont = 1;
-        for(int r = 0; r < rows; r++){
-            for(int c = 0; c < columns; c++){
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < columns; c++) {
                 bidArrayTemplate[r][c] = cont;
                 cont++;
             }
         }
-        return  bidArrayTemplate;
+        return bidArrayTemplate;
     }
 
     public static int[][][] createAndPopulateTridimensionalArray(int depth, int rows, int columns) {
         int[][][] tridArrayTemplate = new int[depth][rows][columns];
         int c = 1;
-        for(int j = 0; j < depth; j++){
-            for(int p = 0; p < rows; p++){
-                for(int a = 0; a < columns; a++){
+        for (int j = 0; j < depth; j++) {
+            for (int p = 0; p < rows; p++) {
+                for (int a = 0; a < columns; a++) {
                     tridArrayTemplate[j][p][a] = c;
                     c++;
                 }
             }
         }
-        return  tridArrayTemplate;
+        return tridArrayTemplate;
     }
 
     public static void main(String[] args) {
